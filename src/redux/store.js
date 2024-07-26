5b5e8b58f180a1e1e90df48b750955e7a158162f
Misha Dorichenko <misha.dorichenko@gmail.com>
2024-07-26 17:55:55 +0300

    remove  Redux Persist code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,47 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
 
-const persistedContactsReducer = persistReducer(
-  {
-    key: 'contacts',
-    storage,
-    whitelist: ['items'],
-  },
-  contactsReducer
-);
-
-// const persistedFiltersReducer = persistReducer(
-//   {
-//     key: 'selectedLang',
-//     storage,
-//     whitelist: ['lang'],
-//   },
-//   filtersReducer
-// );
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
